e898a06d382c76101323a9231c402c74a82d174a
macbook <kienhayb.21091997@gmail.com>
2019-08-19 17:06:43 +0700

    bai kiem tra

---
+++ src/main/java/com/codegym/controller/CityController.java
@@ -0,0 +1,104 @@
+package com.codegym.controller;
+
+
+import com.codegym.model.City;
+import com.codegym.model.Country;
+import com.codegym.service.CityService;
+import com.codegym.service.CountryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/city")
+public class CityController {
+    @Autowired
+    private CityService cityService;
+
+    @Autowired
+    private CountryService countryService;
+
+    @ModelAttribute("countries")
+    public Iterable<Country> countries() {
+        return countryService.findAll();
+    }
+
+    @GetMapping("/list")
+    public ModelAndView listCity() {
+        Iterable<City> cities = cityService.findAll();
+        ModelAndView modelAndView = new ModelAndView("/city/list");
+        modelAndView.addObject("cities", cities);
+        return modelAndView;
+    }
+
+    @GetMapping("/create")
+    public ModelAndView showCreateForm() {
+        ModelAndView modelAndView = new ModelAndView("/city/create");
+        modelAndView.addObject("city", new City());
+        return modelAndView;
+    }
+
+    @PostMapping("/save")
+    public ModelAndView saveCity(@ModelAttribute("city") City city) {
+        cityService.save(city);
+        ModelAndView modelAndView = new ModelAndView(("/city/create"));
+        modelAndView.addObject("city", new City());
+        modelAndView.addObject("message", "new city created");
+        return modelAndView;
+    }
+
+    @GetMapping("/edit/{id}")
+    public ModelAndView editCityForm(@PathVariable Long id) {
+        City city = cityService.findById(id);
+        if (city != null) {
+            ModelAndView modelAndView = new ModelAndView("/city/edit");
+            modelAndView.addObject("city", city);
+            return modelAndView;
+        } else {
+            ModelAndView modelAndView = new ModelAndView("/error-404");
+            return modelAndView;
+        }
+    }
+
+    @PostMapping("/update")
+    public ModelAndView updateCity(@ModelAttribute("city") City city) {
+        cityService.save(city);
+        ModelAndView modelAndView = new ModelAndView("/city/edit");
+        modelAndView.addObject("city", city);
+        modelAndView.addObject("message", "updated successfully ");
+        return modelAndView;
+    }
+
+    @GetMapping("/delete/{id}")
+    public ModelAndView deleteCity(@PathVariable Long id) {
+        City city = cityService.findById(id);
+        if (city != null) {
+            ModelAndView modelAndView = new ModelAndView("/city/delete");
+            modelAndView.addObject("city", city);
+            return modelAndView;
+        } else {
+            ModelAndView modelAndView = new ModelAndView("/error-404");
+            return modelAndView;
+        }
+    }
+
+    @PostMapping("/remove")
+    public String removeCity(@ModelAttribute("city") City city) {
+        cityService.remove(city.getId());
+       return "redirect:list";
+    }
+
+    @GetMapping("/view/{id}")
+    public ModelAndView viewCityForm(@PathVariable Long id) {
+        City city = cityService.findById(id);
+        if (city != null) {
+            ModelAndView modelAndView = new ModelAndView("/city/view");
+            modelAndView.addObject("city", city);
+            return modelAndView;
+        } else {
+            ModelAndView modelAndView = new ModelAndView("/error-404");
+            return modelAndView;
+        }
+    }
+}
